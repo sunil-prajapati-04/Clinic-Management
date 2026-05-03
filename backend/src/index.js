db39dd97101c5bb4ebec2592ba8ccb069750f2de
Sunil Prajapati <2023cssunil14308@poornima.edu.in>
2026-05-03 21:22:27 +0530

    Added Deployed Domains

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ const port = process.env.PORT;
 app.use(helmet());
 
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://clinic-management-dusky.vercel.app',
   credentials:true
 }));
 
